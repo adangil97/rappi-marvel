77f25c820b6bbaf897d38e5a3a42c0fff75f9f63
Adán Gilberto Castillo Vargas <adan.castillo@banregio.com>
2022-08-30 23:09:26 -0500

    Initial commit

---
+++ app/src/main/java/com/rappi/marvel/MarvelApplication.kt
@@ -0,0 +1,10 @@
+package com.rappi.marvel
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author Adán Castillo.
+ */
+@HiltAndroidApp
+class MarvelApplication : Application()
