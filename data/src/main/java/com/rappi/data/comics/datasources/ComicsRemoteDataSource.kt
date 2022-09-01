68784b344547d9d0606319e4d6d1a82c9e8b3973
Adán Gilberto Castillo Vargas <adan.castillo@banregio.com>
2022-08-31 22:18:16 -0500

    Se integran parametros faltantes a peticiones de api marvel

--- data/src/main/java/com/rappi/data/comics/datasources/ComicsRemoteDataSource.kt
+++ data/src/main/java/com/rappi/data/comics/datasources/ComicsRemoteDataSource.kt
@@ -1,10 +1,12 @@
 package com.rappi.data.comics.datasources
 
+import com.rappi.data.DataConstants
 import com.rappi.domain.comics.Comic
 import com.rappi.domain.comics.ComicDataWrapper
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import java.security.MessageDigest
 
 /**
  * @author Adán Castillo.
@@ -15,7 +17,17 @@ abstract class ComicsRemoteDataSource(
 
     suspend fun getComics(offset: Int): List<Comic> {
         val response: ComicDataWrapper = client.get("comics") {
+            val md = MessageDigest.getInstance("MD5")
+            val currentTime = System.currentTimeMillis()
             parameter("offset", offset)
+            parameter("ts", currentTime)
+            parameter("apikey", DataConstants.PUBLIC_KEY)
+            parameter(
+                "hash",
+                md.digest(
+                    "$currentTime${DataConstants.PUBLIC_KEY}${DataConstants.PRIVATE_KEY}".toByteArray()
+                ).toString()
+            )
         }.body()
         return response.data.results
     }
--- data/src/main/java/com/rappi/data/series/datasources/SeriesRemoteDataSource.kt
+++ data/src/main/java/com/rappi/data/series/datasources/SeriesRemoteDataSource.kt
@@ -1,10 +1,12 @@
 package com.rappi.data.series.datasources
 
+import com.rappi.data.DataConstants
 import com.rappi.domain.series.Series
 import com.rappi.domain.series.SeriesDataWrapper
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import java.security.MessageDigest
 
 /**
  * @author Adán Castillo.
@@ -15,7 +17,17 @@ abstract class SeriesRemoteDataSource(
 
     suspend fun getSeries(offset: Int): List<Series> {
         val response: SeriesDataWrapper = client.get("series") {
+            val md = MessageDigest.getInstance("MD5")
+            val currentTime = System.currentTimeMillis()
             parameter("offset", offset)
+            parameter("ts", currentTime)
+            parameter("apikey", DataConstants.PUBLIC_KEY)
+            parameter(
+                "hash",
+                md.digest(
+                    "$currentTime${DataConstants.PUBLIC_KEY}${DataConstants.PRIVATE_KEY}".toByteArray()
+                ).toString()
+            )
         }.body()
         return response.data.results
     }
