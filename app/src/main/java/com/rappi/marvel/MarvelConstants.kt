22b24b7dbd8431afbd5ead476ecea44f5e3ec580
Adán Gilberto Castillo Vargas <adan.castillo@banregio.com>
2022-08-31 23:01:57 -0500

    Se agrega inyección de dependencias global.

---
+++ app/src/main/java/com/rappi/marvel/MarvelConstants.kt
@@ -0,0 +1,9 @@
+package com.rappi.marvel
+
+/**
+ * @author Adán Castillo.
+ */
+object MarvelConstants {
+
+    const val BASE_URL = "gateway.marvel.com:443/v1/public/"
+}
---
+++ app/src/main/java/com/rappi/marvel/MarvelModule.kt
@@ -0,0 +1,55 @@
+package com.rappi.marvel
+
+import android.content.Context
+import androidx.room.Room
+import com.rappi.marvel.database.MarvelDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+/**
+ * @author Adán Castillo.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object MarvelModule {
+
+    @Provides
+    @Singleton
+    fun providesMarvelDatabase(@ApplicationContext context: Context): MarvelDatabase =
+        Room.databaseBuilder(
+            context,
+            MarvelDatabase::class.java,
+            MarvelDatabase.DATABASE_NAME
+        ).fallbackToDestructiveMigration().build()
+
+    @Provides
+    @Singleton
+    fun providesHttpClient(): HttpClient =
+        HttpClient(OkHttp) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+            expectSuccess = true
+            defaultRequest {
+                host = MarvelConstants.BASE_URL
+                url {
+                    protocol = URLProtocol.HTTPS
+                }
+            }
+        }
+}
