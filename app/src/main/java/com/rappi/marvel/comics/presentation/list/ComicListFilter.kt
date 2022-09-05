39d062c53f90df0d9f16c2e0991810f480fa7f8e
Adán Gilberto Castillo Vargas <adan.castillo@banregio.com>
2022-09-05 11:33:01 -0500

    Se remuven unused

--- app/src/main/java/com/rappi/marvel/comics/presentation/list/ComicListFilter.kt
+++
@@ -1,9 +0,0 @@
-package com.rappi.marvel.comics.presentation.list
-
-/**
- * @author Adán Castillo.
- */
-data class ComicListFilter(
-    val page: Int,
-    val requireMoreData: Boolean = false
-)
--- app/src/main/java/com/rappi/marvel/series/presentation/list/SeriesListFilter.kt
+++
@@ -1,9 +0,0 @@
-package com.rappi.marvel.series.presentation.list
-
-/**
- * @author Adán Castillo.
- */
-data class SeriesListFilter(
-    val page: Int,
-    val requireMoreData: Boolean = false
-)
